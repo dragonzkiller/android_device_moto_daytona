0105ee6304868345fe6a9e598e930aeee367a0bc
Bryan Gantt <dragonzkiller@gmail.com>
2012-06-26 15:25:12 -0500

    added hijack

---
+++ hijack/hijack-boot/start.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <cutils/properties.h>
+
+int main(int argc, char *argv[])
+{
+    char buf[1024];
+    if(argc > 1) {
+        property_set("ctl.start", argv[1]);
+    } else {
+        /* default to "start zygote" "start runtime" */
+        property_set("ctl.start", "zygote");
+        property_set("ctl.start", "runtime");
+    }
+    
+    return 0;
+}
